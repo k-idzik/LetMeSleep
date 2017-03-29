03e382987a315179854da66b0525dc1c161b1b3f
Kevin Idzik <kdi1996@live.com>
2017-03-29 12:53:15 -0400

    Added Pause When Leaving Window
    -Canvas dimensions will be 450x800 for 1920x1080 resolution for now

--- scripts/loader.js
+++ scripts/loader.js
@@ -3,8 +3,19 @@
 //Use the existing app if it does exist, otherwise make a new object literal
 var app = app || {};
 
+//When the page loads, begin the game
 window.onload = function() {
     //Load resources here
     
     app.main.init(); //Begin the game
+}
+
+//When the window running the game leaves focus
+window.onblur = function() {
+    app.main.pauseGame(); //Pause the game
+}
+
+//When the window running the game returns to focus
+window.onfocus = function() {
+    app.main.resumeGame(); //Resume the game
 }
--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,9 @@ app.main = {
     canvas: undefined,
     ctx: undefined,
     animationID: 0,
+    paused: false,
 
-    //Initialization function
+    ///Initialization function
     init: function() {
         this.canvas = document.querySelector("canvas");
         this.ctx = canvas.getContext("2d");
@@ -17,12 +18,55 @@ app.main = {
         this.update(); //Start the animation loop
     },
 
-    //Update
+    ///Update, runs the game
     update: function() {
         //Update the animation frame 60 times a second, binding it to call itself
         this.animationID = requestAnimationFrame(this.update.bind(this));
         
-        this.ctx.fillStyle = "red";
-        this.ctx.fillRect(0, 0, 150, 150);
+        this.ctx.fillRect(0, 0, this.canvas.height, this.canvas.width); //Clear the background
+        
+        //If the game is paused
+        if (this.paused) {
+            this.drawPauseScreen(this.ctx);
+            return; //Skip the rest of the loop
+        }
+    },
+    
+    ///This function will pause the game
+    pauseGame: function() {
+        this.paused = true;      
+        cancelAnimationFrame(this.animationID); //Stop the animation loop
+        this.update(); //Updates the screen once so that the pause screen is shown
+    },
+    
+    ///This function will resume the game after pause
+    resumeGame: function() {
+        cancelAnimationFrame(this.animationID); //Stop the animation loop in case it's running
+        this.paused = false;
+        this.update(); //Restart the loop
+    },
+    
+    ///This function will draw the pause screen
+    drawPauseScreen: function(ctx) {
+        ctx.save(); //Save the current state of the canvas
+        
+        //TODO: Update later with menu options
+        
+        //Obscure the game
+        ctx.globalAlpha = .75;
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        
+        //Align the text on the screen
+        ctx.translate(this.canvas.width / 2, this.canvas.height / 4);
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        
+        //Draw text
+        ctx.font = "40pt Open Sans";
+        ctx.fillStyle = "white";
+        ctx.fillText("PAUSED", 0, 0);
+        
+        ctx.restore(); //Restore the canvas state to what it was before drawing the pause screen
     }
 };
